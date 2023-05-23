c5c801359740f91b553783433d23c040639b75a0
pauline <vdhpauline@hotmail.com>
2023-05-23 17:44:29 +0200

    comment

--- reseau_interne/web/html/index.php
+++ reseau_interne/web/html/index.php
@@ -17,10 +17,8 @@
           mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
           // Connect to the database
-          //$conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
-          $conn = new mysqli('192.168.0.3', 'root', 'user123', 'woodytoys');
-
-
+          $conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
+        
           // Check connection
           if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
