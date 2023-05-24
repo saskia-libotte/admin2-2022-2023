e81b8f2541b433df7cbe654578b197e6e7ee978f
pauline <vdhpauline@hotmail.com>
2023-05-24 09:52:57 +0200

    change

--- reseau_interne/web/html/index.php
+++ reseau_interne/web/html/index.php
@@ -17,7 +17,8 @@
           mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
           // Connect to the database
-          $conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
+          //$conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
+          $conn = new MySQLi('192.168.0.3', 'root', 'user123');
         
           // Check connection
           if ($conn->connect_error) {
