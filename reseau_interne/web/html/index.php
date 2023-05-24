be13e3eb0e213a3439c1a188c3bbdc224ce26bbb
pauline <vdhpauline@hotmail.com>
2023-05-24 09:31:11 +0200

    change

--- reseau_interne/web/html/index.php
+++ reseau_interne/web/html/index.php
@@ -21,7 +21,7 @@
         
           // Check connection
           if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+            die("Connection failed: ".$conn->connect_error);
           }
 
           // Query the Products table
