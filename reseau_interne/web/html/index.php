11c3cf08ddfb1278b9dd8d16be9cec2945145ae2
pauline <vdhpauline@hotmail.com>
2023-05-24 10:32:40 +0200

    change

--- reseau_interne/web/html/index.php
+++ reseau_interne/web/html/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Product</title>
+    <title>Marchand</title>
   </head>
   <body>
     <table>
       <thead>
         <tr>
-          <th>id</th>
-          <th>nom</th>
-          <th>mail</th>
+          <th>ID</th>
+          <th>Nom</th>
+          <th>Email</th>
         </tr>
       </thead>
       <tbody>
@@ -18,40 +18,35 @@
 
         try {
           // Connect to the database
-          //$conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
           $conn = new MySQLi('192.168.0.3', 'root', 'user123');
 
-          // Check connection
-          if ($conn->connect_error) {
-              echo "Not connected successfully";
+          // Select the database
+          $conn->select_db('woodytoys');
+
+          // Query the Customers table
+          $sql = "SELECT * FROM Customers";
+          $stmt = $conn->prepare($sql);
+          $stmt->execute();
+          $result = $stmt->get_result();
+
+          // Output each row as a table row
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              echo "<tr>";
+              echo "<td>" . $row["ID"] . "</td>";
+              echo "<td>" . $row["Nom"] . "</td>";
+              echo "<td>" . $row["Email"] . "</td>";
+              echo "</tr>";
+            }
           } else {
-              echo "Connected successfully";
-
-              // Select the database
-              $conn->select_db('woodytoys');
-
-              // Query the Customers table
-              $sql = "SELECT * FROM Customers";
-              $stmt = $conn->prepare($sql);
-              $stmt->execute();
-              $result = $stmt->get_result();
-
-              // Output each row as a table row
-              if ($result->num_rows > 0) {
-                  echo "<table>";
-                  while ($row = $result->fetch_assoc()) {
-                      echo "<tr>";
-                      echo "<td>" . $row["ID"] . "</td>";
-                      echo "<td>" . $row["Nom"] . "</td>";
-                      echo "<td>" . $row["Email"] . "</td>";
-                      echo "</tr>";
-                  }
-                  echo "</table>";
-              }
-
-              // Close the database connection
-              $conn->close();
+            echo "<tr><td colspan='3'>Aucun enregistrement trouvé</td></tr>";
           }
+
+          // Close the database connection
+          $conn->close();
+        } catch (Exception $e) {
+          echo "Erreur de connexion à la base de données : " . $e->getMessage();
+        }
         ?>
       </tbody>
     </table>
