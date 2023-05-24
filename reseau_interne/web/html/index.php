d331ceeb0490b1df028e67fa87af648696061b56
pauline <vdhpauline@hotmail.com>
2023-05-24 10:21:48 +0200

    change

--- reseau_interne/web/html/index.php
+++ reseau_interne/web/html/index.php
@@ -17,33 +17,40 @@
           mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
           // Connect to the database
-          //$conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
           $conn = new MySQLi('192.168.0.3', 'root', 'user123');
-        
+
           // Check connection
-          if ($conn -> connect_error) {;
-            echo "NOT Connected successfully";;
-            }
-            echo "Connected successfully";
-            
-          // Query the Customers table
-          $sql = "SELECT * FROM Customers";
-          $result = $conn->query($sql);
+          if ($conn->connect_error) {
+              echo "Not connected successfully";
+          } else {
+              echo "Connected successfully";
 
-          // Output each row as a table row
-          if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-              echo "<tr>";
-              echo "<td>" . $row["ID"] . "</td>";
-              echo "<td>" . $row["Nom"] . "</td>";
-              echo "<td>" . $row["Email"] . "</td>";
-              echo "</tr>";
-            }
+              // Select the database
+              $conn->select_db('woodytoys');
+
+              // Query the Customers table
+              $sql = "SELECT * FROM Customers";
+              $stmt = $conn->prepare($sql);
+              $stmt->execute();
+              $result = $stmt->get_result();
+
+              // Output each row as a table row
+              if ($result->num_rows > 0) {
+                  echo "<table>";
+                  while ($row = $result->fetch_assoc()) {
+                      echo "<tr>";
+                      echo "<td>" . $row["ID"] . "</td>";
+                      echo "<td>" . $row["Nom"] . "</td>";
+                      echo "<td>" . $row["Email"] . "</td>";
+                      echo "</tr>";
+                  }
+                  echo "</table>";
+              }
+
+              // Close the database connection
+              $conn->close();
           }
-
-          // Close the database connection
-          //$conn->close();
-        ?>
+  ?>
       </tbody>
     </table>
   </body>
