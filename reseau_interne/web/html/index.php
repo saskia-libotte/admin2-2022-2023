7d08b9645004ebe767516dfd7bfc81a7aa168d65
simonNolf <simon.nolf@gmail.com>
2023-05-24 10:27:10 +0200

    test sth

--- reseau_interne/web/html/index.php
+++ reseau_interne/web/html/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Marchand</title>
+    <title>Product</title>
   </head>
   <body>
     <table>
       <thead>
         <tr>
-          <th>ID</th>
-          <th>Nom</th>
-          <th>Email</th>
+          <th>Produit</th>
+          <th>quantités</th>
+          <th>Prix</th>
         </tr>
       </thead>
       <tbody>
@@ -17,39 +17,33 @@
           mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
           // Connect to the database
+          //$conn = mysqli_connect('192.168.0.3', 'root', 'user123', 'woodytoys');
           $conn = new MySQLi('192.168.0.3', 'root', 'user123');
-
+        
           // Check connection
-          if ($conn->connect_error) {
-              echo "Not connected successfully";
-          } else {
-              echo "Connected successfully";
+          if ($conn -> connect_error) {;
+            echo "NOT Connected successfully";;
+            }
+            echo "Connected successfully";
+          $conn->select_db('woodytoys');
+            
+          // Query the Products table
+          $sql = "SELECT * FROM Products";
+          $result = $conn->query($sql);
 
-              // Select the database
-              $conn->select_db('woodytoys');
-
-              // Query the Customers table
-              $sql = "SELECT * FROM Customers";
-              $stmt = $conn->prepare($sql);
-              $stmt->execute();
-              $result = $stmt->get_result();
-
-              // Output each row as a table row
-              if ($result->num_rows > 0) {
-                  echo "<table>";
-                  while ($row = $result->fetch_assoc()) {
-                      echo "<tr>";
-                      echo "<td>" . $row["ID"] . "</td>";
-                      echo "<td>" . $row["Nom"] . "</td>";
-                      echo "<td>" . $row["Email"] . "</td>";
-                      echo "</tr>";
-                  }
-                  echo "</table>";
-              }
-
-              // Close the database connection
-              $conn->close();
+          // Output each row as a table row
+          if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+              echo "<tr>";
+              echo "<td>" . $row["Produit"] . "</td>";
+              echo "<td>" . $row["Quantite"] . "</td>";
+              echo "<td>" . $row["Prix"] . "</td>";
+              echo "</tr>";
+            }
           }
+
+          // Close the database connection
+          //$conn->close();
         ?>
       </tbody>
     </table>
