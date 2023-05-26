6c41f805925d48f171ccc3711e8646bfb5a65117
simonNolf <simon.nolf@gmail.com>
2023-05-26 11:28:51 +0200

    change web public

--- reseau_externe/web/sites/php/index.php
+++ reseau_externe/web/sites/php/index.php
@@ -6,18 +6,7 @@
     
 </head>
 <body>
-    <h2>Ajouter un jouet !</h2>
-    <form method="POST" action="?">
-        <input type="search" placeholder="Nom du jouet" name="nomDuProduit">
-        <input type="search" placeholder="Prix du jouet" name="prixDuProduit">
-        <button type="submit">Ajouter le nouveau jouet !</button>
-    </form>
-
-    <h2>Supprimer un jouet !</h2>
-    <form method="POST" action="?">
-        <input type="search" placeholder="Nom du jouet à supprimer" name="nomDuProduitASupprimer">
-        <button type="submit">Supprimer le jouet</button>
-    </form>
+    <h1> voici les données de la base de données.
 
     <?php
 
@@ -52,45 +41,6 @@
 
         // Fermeture de la connexion
         $conn->close();
-
-
-    // Handle form submission for adding a toy
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nomDuProduit']) && isset($_POST['prixDuProduit'])) {
-        $nomProduit = $_POST['nomDuProduit'];
-        $prixDuProduit = $_POST['prixDuProduit'];
-
-        // Check if the toy already exists
-        $checkSql = "SELECT * FROM cadeau WHERE id='$nomProduit'";
-        $checkResult = $conn->query($checkSql);
-        if ($checkResult->num_rows > 0) {
-            echo "<p class='error'>Ce jouet existe déjà !</p>";
-        } else {
-            // Insert new toy into the database
-            $insertSql = "INSERT INTO cadeau (name, price) VALUES ('$nomProduit', $quantiteDuProduit)";
-            if ($conn->query($insertSql) === TRUE) {
-                echo "<p class='success'>Jouet ajouté avec succès !</p>";
-                // Refresh the page to update the id list
-                echo "<meta http-equiv='refresh' content='0'>";
-            } else {
-                echo "<p class='error'>Erreur lors de l'ajout du jouet : " . $conn->error . "</p>";
-            }
-        }
-    }
-
-    // Handle form submission for deleting a toy
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nomDuProduitASupprimer'])) {
-        $nomProduitASupprimer = $_POST['nomDuProduitASupprimer'];
-
-        // Delete toy from the database
-        $deleteSql = "DELETE FROM cadeau WHERE name='$nomProduitASupprimer'";
-        if ($conn->query($deleteSql) === TRUE) {
-            echo "<p class='success'>Jouet supprimé avec succès !</p>";
-            // Refresh the page to update the id list
-            echo "<meta http-equiv='refresh' content='0'>";
-        } else {
-            echo "<p class='error'>Erreur lors de la suppression du jouet : " . $conn->error . "</p>";
-        }
-    }
     ?>
 
 </body>
